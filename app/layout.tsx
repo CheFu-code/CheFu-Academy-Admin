f49246c74556163bd7109ea53add67a466c12293
CheFu <kurisanimaluleke77@gmail.com>
2026-01-29 11:13:35 +0200

    feat: Add Google Ads script to RootLayout for ad integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@/components/ui/theme-provider';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -30,6 +31,14 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
+            <head>
+                <Script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2568872335031834"
+                    crossOrigin="anonymous"
+                    strategy="afterInteractive"
+                ></Script>
+            </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
